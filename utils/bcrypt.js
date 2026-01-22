2cbe415e4edd096af0a433fe5c9b138e62f33b8b
rimpleshrestha <rimpleshrestha133@gmail.com>
2026-01-22 18:36:27 +0545

    password hashing

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -1,9 +1,14 @@
 const bcrypt = require("bcrypt");
 
 const encryptPassword = async (password) => {
-  return await bcrypt.hashSync(password, Number(process.env.SALT));
+  const saltRounds = Number(process.env.SALT) || 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
 };
+
 const comparePassword = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 };
+
 module.exports = { encryptPassword, comparePassword };
